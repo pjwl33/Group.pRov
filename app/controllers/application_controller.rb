ee653dc2b3f66b045544fe01ad687b6cb7932df4
pjwl33 <paul.leejw@gmail.com>
2014-05-10 16:59:34 -0400

    facebook oauth/user create working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :signed_in?, :current_user
+
+  private
+
+  #checking for user signed in
+  def signed_in?
+    true if session[:user_id]
+  end
+
+  #current user object return
+  def current_user
+    if signed_in?
+      User.find(session[:user_id])
+    else
+      redirect_to root_path
+    end
+  end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
+  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_SECRET_KEY']
+end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
 end
