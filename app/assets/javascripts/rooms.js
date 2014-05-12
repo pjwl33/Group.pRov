a6b4322002c104ba97de532bfeb08994276b117a
pjwl33 <paul.leejw@gmail.com>
2014-05-12 16:36:50 -0400

    sound fxns working for piano

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -1,5 +1,25 @@
-function instrument() {
-  var AudioContext = AudioContext || webkitAudioContext;
-  var context = new AudioContext();
-  var buffer;
+function instrument(type) {
+  var context = new webkitAudioContext();
+  var soundFiles;
+  var keyCodes = [81, 50, 87, 51, 69, 82, 53, 84, 54, 89, 55, 85];
+  if (type == "keyboard") {
+    soundFiles = ['piano_c.wav', 'piano_cs.wav', 'piano_d.wav', 'piano_eb.wav', 'piano_e.wav', 'piano_f.wav', 'piano_fs.wav', 'piano_g.wav', 'piano_gs.wav', 'piano_a.wav', 'piano_bb.wav', 'piano_b.wav'];
+  } else if (type == "hip-hop-beats") {
+
+  }
+  for (i = 0; i < soundFiles.length; i++) {
+    var audio = new Audio();
+    audio.src = 'https://s3.amazonaws.com/s3-ex-bucket/' + soundFiles[i];
+    audio.id = soundFiles[i];
+    var source = context.createMediaElementSource(audio);
+    source.connect(context.destination);
+    $('#sounds').append(audio);
+    $(document).keydown(function(e) {
+      if (e.keyCode == keyCodes[i]) {
+        document.getElementById(soundFiles[i]).play();
+        return false;
+      }
+    });
+  }
 }
+
