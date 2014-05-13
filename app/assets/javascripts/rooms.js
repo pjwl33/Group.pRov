af9040885a5a561ebf08597665e20e5ed99cbc31
pjwl33 <paul.leejw@gmail.com>
2014-05-13 09:21:06 -0400

    Starting day 3

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -26,9 +26,40 @@ function instrument(type) {
 }
 
 function tester() {
-  var context = new AudioContext;
-  var oscillator = context.createOscillator();
-  oscillator.frequency.value = 200;
-  oscillator.connect(context.destination);
-  // oscillator.start(0);
+  var keyboard = QwertyHancock({id: 'keyboard'});
+
+  var context = new AudioContext();
+
+  /* VCO */
+  var vco = context.createOscillator();
+  vco.type = vco.SINE;
+  vco.frequency.value = this.frequency;
+  vco.start(0);
+
+  /* VCA */
+  var vca = context.createGain();
+  vca.gain.value = 0;
+
+  /* Connections */
+  vco.connect(vca);
+  vca.connect(context.destination);
+
+  var isEmpty = function(obj) {
+    return Object.keys(obj).length === 0;
+  };
+
+  var depressed_keys = {};
+
+  keyboard.keyDown(function (note, frequency) {
+    vco.frequency.value = frequency;
+    vca.gain.value = 1;
+    depressed_keys[note] = true;
+  });
+
+  keyboard.keyUp(function (note, _) {
+    delete depressed_keys[note];
+    if (isEmpty(depressed_keys)) {
+      vca.gain.value = 0;
+    }
+  });
 }
