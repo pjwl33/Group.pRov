27dd8f692ca6dfc7b6471fa37407ea3b0cfacac1
pjwl33 <paul.leejw@gmail.com>
2014-05-27 21:01:16 -0400

    how to page added

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,6 +3,7 @@ class Track < ActiveRecord::Base
   belongs_to :user
   belongs_to :room
   validates_presence_of :sequence
+  validates_uniqueness_of :sequence
   validates_presence_of :instrument
 
 end
